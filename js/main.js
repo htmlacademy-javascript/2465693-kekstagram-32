81db3e877c9927f70542341686a762bd9f251421
VslavV <um.w@mail.ru>
2024-07-05 16:52:38 +0500

    Рефакторинг. Изменение констант, использованием только стрелочных функций, генерация массива комментариев без замыкания.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 const NUMBERS_OF_PHOTOS = 25;
-const MIN_NUMBER_AVATAR = 1;
-const MAX_NUMBER_AVATAR = 6;
-const MIN_COUNTS_LIKES = 15;
-const MAX_COUNTS_LIKES = 300;
-const MAX_COUNTS_COMMENTS = 30;
+const NUMBER_AVATAR = {
+  MIN: 1,
+  MAX: 6,
+};
+
+const COUNTS_LIKES = {
+  MIN: 15,
+  MAX: 30,
+};
+
+const MAX_COUNTS_COMMENTS = 300;
 const MAX_COUNTS_ID_COMMENTS = 500;
 
-const descriptionPhoto = [
+const descriptionPhotos = [
   'Галерея пляжа Мулини',
   'сходи на пляж',
   'пляж карибского моря',
@@ -57,24 +63,15 @@ const messageComments = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-function createIdGenerator() {
-  let lastGeneratedId = 0;
-
-  return function () {
-    lastGeneratedId += 1;
-    return lastGeneratedId;
-  };
-}
-
-function getRandomInteger(min, max) {
+const getRandomInteger = (min, max) => {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const result = Math.random() * (upper - lower + 1) + lower;
 
   return Math.floor(result);
-}
+};
 
-function createRandomIdFromRangeGenerator(min, max) {
+const createRandomIdFromRangeGenerator = (min, max) => {
   const previousValues = [];
 
   return function () {
@@ -85,61 +82,47 @@ function createRandomIdFromRangeGenerator(min, max) {
     previousValues.push(currentValue);
     return currentValue;
   };
-}
+};
 
-const getRandomArrayElement = (elements) =>
-  elements[getRandomInteger(0, elements.length - 1)];
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const generateCommentId = createRandomIdFromRangeGenerator(
-  1,
-  MAX_COUNTS_ID_COMMENTS
-);
+const generateCommentId = createRandomIdFromRangeGenerator(1, MAX_COUNTS_ID_COMMENTS);
 
-function createComments() {
-  return {
-    id: generateCommentId(),
-    avatar: `img/avatar-${getRandomInteger(
-      MIN_NUMBER_AVATAR,
-      MAX_NUMBER_AVATAR
-    )}.svg`,
-    message: getRandomArrayElement(messageComments),
-    name: getRandomArrayElement(nameAuthorComments),
-  };
-}
+const createComments = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInteger(NUMBER_AVATAR.MIN, NUMBER_AVATAR.MAX)}.svg`,
+  message: getRandomArrayElement(messageComments),
+  name: getRandomArrayElement(nameAuthorComments),
+});
 
-function createArrayComments() {
-  let lastArrayComments = [];
+const createArrayComments = () => {
+  const lastArrayComments = [];
 
-  return function () {
-    lastArrayComments = Array.from(
-      { length: getRandomInteger(0, MAX_COUNTS_COMMENTS) },
-      createComments
-    );
-    return lastArrayComments;
-  };
-}
+  for (let i = 0; i < getRandomInteger(0, MAX_COUNTS_COMMENTS); i++) {
+    const comments = createComments();
+    lastArrayComments.push(comments);
+  }
 
-const generateArrayComments = createArrayComments();
+  return lastArrayComments;
+};
 
-const generatePhotoId = createIdGenerator();
+const generateArrayComments = createArrayComments;
 
-function createPhotoId() {
+const generatePhotoId = createRandomIdFromRangeGenerator(1, NUMBERS_OF_PHOTOS);
+
+const createPhotoId = () => {
   const photoId = generatePhotoId();
-  const currentArraycomments = generateArrayComments();
 
   return {
     id: photoId,
     url: `photos/${photoId}.jpg`,
-    description: descriptionPhoto[photoId - 1],
-    likes: getRandomInteger(MIN_COUNTS_LIKES, MAX_COUNTS_LIKES),
-    comments: currentArraycomments,
+    description: descriptionPhotos[photoId - 1],
+    likes: getRandomInteger(COUNTS_LIKES.MIN, COUNTS_LIKES.MAX),
+    comments: generateArrayComments(),
   };
-}
+};
 
-const createArrayPhotos = Array.from(
-  { length: NUMBERS_OF_PHOTOS },
-  createPhotoId
-);
+const createArrayPhotos = Array.from({ length: NUMBERS_OF_PHOTOS }, createPhotoId);
 
 // eslint-disable-next-line no-console
 console.log(createArrayPhotos);
