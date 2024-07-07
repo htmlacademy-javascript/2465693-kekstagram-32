922a499b74b82f98e50d2211c65a446a1955f08c
VslavV <um.w@mail.ru>
2024-07-07 13:36:53 +0500

    Практическое задание 5.2: Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -53,3 +53,38 @@ extractNumber('а я томат'); // NaN
 extractNumber(2023); // 2023
 extractNumber(-1); // 1
 extractNumber(1.5); // 15
+
+//Практическое зхадание №5
+
+const dayStart = '08:0';
+const dayEnd = '17:30';
+const startMeeting = '08:00';
+const duration = '90';
+
+const getMinutes = (time) => {
+  const result = time.split(':');
+  const resultInteger = [];
+  let minutes = 0;
+  for (let i = 0; i <= result.length - 1; i++) {
+    resultInteger[i] = parseInt(result[i], 10);
+  }
+  if (resultInteger[1] === 0) {
+    minutes = resultInteger[0] * 60;
+  } else {
+    minutes = resultInteger[0] * 60 + resultInteger[1];
+  }
+  return minutes;
+};
+
+const planer = (start, end, meeting, durat) => {
+  const durationMeeting = getMinutes(end) - getMinutes(meeting) - parseInt(durat, 10);
+  const meetingEarlier = getMinutes(meeting) - getMinutes(start);
+  if (durationMeeting > 0 || meetingEarlier > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// eslint-disable-next-line no-console
+console.log(planer(dayStart, dayEnd, startMeeting, duration));
