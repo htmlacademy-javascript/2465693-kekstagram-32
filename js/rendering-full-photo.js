71fc29cac5ea435d59e5669945c84cada79961f4
VslavV <um.w@mail.ru>
2024-07-26 01:01:05 +0500

    ДЗ 8.15. Открывается и закрывается (часть 2)

--- js/rendering-full-photo.js
+++ js/rendering-full-photo.js
@@ -4,12 +4,9 @@ import { displayComments } from './display-comments.js';
 const renderBigPhoto = (arrayPhotos) => {
   const bigPictureElement = document.querySelector('.big-picture');
   const bigPictureCloseElement = bigPictureElement.querySelector('.big-picture__cancel');
-  const commentCountElement = document.querySelector('.social__comment-count');
-  const commentLoaderElement = document.querySelector('.comments-loader');
+  // const commentCountElement = document.querySelector('.social__comment-count');
+  // const commentLoaderElement = document.querySelector('.comments-loader');
   const containerPhotoElement = document.querySelector('.pictures');
-
-  commentCountElement.classList.add('hidden');
-  commentLoaderElement.classList.add('hidden');
 
   //закрытие по ESC
   const onDocumentEscKeydown = (evt) => {
@@ -36,12 +33,12 @@ const renderBigPhoto = (arrayPhotos) => {
   //получаем данные для большого фото
   const getDataBigPhoto = (photoId) => {
     const index = arrayPhotos.findIndex((photo) => photoId === photo.id.toString());
-    const { url, likes, comments, description } = arrayPhotos[index];
+    const { url, likes, description } = arrayPhotos[index];
 
     bigPictureElement.querySelector('.big-picture__img img').src = url;
     bigPictureElement.querySelector('.likes-count').textContent = likes;
-    bigPictureElement.querySelector('.social__comment-shown-count').textContent = comments.length;
-    bigPictureElement.querySelector('.social__comment-total-count').textContent = comments.length;
+    // bigPictureElement.querySelector('.social__comment-shown-count').textContent = comments.length;
+    // bigPictureElement.querySelector('.social__comment-total-count').textContent = comments.length;
     bigPictureElement.querySelector('.social__caption').textContent = description;
     displayComments(arrayPhotos[index].comments);
   };
